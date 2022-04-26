25b13d038bb74268c4d32f7e647798c153c20b65
yanpliu <yanpliu@redhat.com>
2022-04-26 15:08:58 +0800

    Update virt-who cases to default_org

--- tests/foreman/virtwho/cli/test_libvirt.py
+++ tests/foreman/virtwho/cli/test_libvirt.py
@@ -31,7 +31,7 @@ from robottelo.virtwho_utils import get_configure_option
 
 
 @pytest.fixture()
-def form_data(default_sat, module_manifest_org):
+def form_data(default_sat, default_org):
     form = {
         'name': gen_string('alpha'),
         'debug': 1,
@@ -39,7 +39,7 @@ def form_data(default_sat, module_manifest_org):
         'hypervisor-id': 'hostname',
         'hypervisor-type': settings.virtwho.libvirt.hypervisor_type,
         'hypervisor-server': settings.virtwho.libvirt.hypervisor_server,
-        'organization-id': module_manifest_org.id,
+        'organization-id': default_org.id,
         'filtering-mode': 'none',
         'satellite-url': default_sat.hostname,
         'hypervisor-username': settings.virtwho.libvirt.hypervisor_username,
@@ -54,7 +54,7 @@ def virtwho_config(form_data):
 
 class TestVirtWhoConfigforLibvirt:
     @pytest.mark.tier2
-    def test_positive_deploy_configure_by_id(self, module_manifest_org, form_data, virtwho_config):
+    def test_positive_deploy_configure_by_id(self, default_org, form_data, virtwho_config):
         """Verify " hammer virt-who-config deploy"
 
         :id: e66bf88a-bd4e-409a-91a8-bc5e005d95dd
@@ -66,9 +66,9 @@ class TestVirtWhoConfigforLibvirt:
         :CaseImportance: High
         """
         assert virtwho_config['status'] == 'No Report Yet'
-        command = get_configure_command(virtwho_config['id'], module_manifest_org.label)
+        command = get_configure_command(virtwho_config['id'], default_org.name)
         hypervisor_name, guest_name = deploy_configure_by_command(
-            command, form_data['hypervisor-type'], debug=True, org=module_manifest_org.label
+            command, form_data['hypervisor-type'], debug=True, org=default_org.label
         )
         virt_who_instance = VirtWhoConfig.info({'id': virtwho_config['id']})['general-information'][
             'status'
@@ -81,7 +81,7 @@ class TestVirtWhoConfigforLibvirt:
         for hostname, sku in hosts:
             host = Host.list({'search': hostname})[0]
             subscriptions = Subscription.list(
-                {'organization': module_manifest_org.label, 'search': sku}
+                {'organization': default_org.name, 'search': sku}
             )
             vdc_id = subscriptions[0]['id']
             if 'type=STACK_DERIVED' in sku:
@@ -96,7 +96,7 @@ class TestVirtWhoConfigforLibvirt:
 
     @pytest.mark.tier2
     def test_positive_deploy_configure_by_script(
-        self, module_manifest_org, form_data, virtwho_config
+        self, default_org, form_data, virtwho_config
     ):
         """Verify " hammer virt-who-config fetch"
 
@@ -111,7 +111,7 @@ class TestVirtWhoConfigforLibvirt:
         assert virtwho_config['status'] == 'No Report Yet'
         script = VirtWhoConfig.fetch({'id': virtwho_config['id']}, output_format='base')
         hypervisor_name, guest_name = deploy_configure_by_script(
-            script, form_data['hypervisor-type'], debug=True, org=module_manifest_org.label
+            script, form_data['hypervisor-type'], debug=True, org=default_org.label
         )
         virt_who_instance = VirtWhoConfig.info({'id': virtwho_config['id']})['general-information'][
             'status'
@@ -124,7 +124,7 @@ class TestVirtWhoConfigforLibvirt:
         for hostname, sku in hosts:
             host = Host.list({'search': hostname})[0]
             subscriptions = Subscription.list(
-                {'organization': module_manifest_org.label, 'search': sku}
+                {'organization': default_org.name, 'search': sku}
             )
             vdc_id = subscriptions[0]['id']
             if 'type=STACK_DERIVED' in sku:
@@ -138,7 +138,7 @@ class TestVirtWhoConfigforLibvirt:
         assert not VirtWhoConfig.exists(search=('name', form_data['name']))
 
     @pytest.mark.tier2
-    def test_positive_hypervisor_id_option(self, module_manifest_org, form_data, virtwho_config):
+    def test_positive_hypervisor_id_option(self, default_org, form_data, virtwho_config):
         """Verify hypervisor_id option by hammer virt-who-config update"
 
         :id: 082a0eec-f024-4605-b876-a8959cf68e0c
@@ -155,9 +155,9 @@ class TestVirtWhoConfigforLibvirt:
             result = VirtWhoConfig.info({'id': virtwho_config['id']})
             assert result['connection']['hypervisor-id'] == value
             config_file = get_configure_file(virtwho_config['id'])
-            command = get_configure_command(virtwho_config['id'], module_manifest_org.label)
+            command = get_configure_command(virtwho_config['id'], default_org.name)
             deploy_configure_by_command(
-                command, form_data['hypervisor-type'], org=module_manifest_org.label
+                command, form_data['hypervisor-type'], org=default_org.label
             )
             assert get_configure_option('hypervisor_id', config_file) == value
         VirtWhoConfig.delete({'name': virtwho_config['name']})
